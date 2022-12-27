937757f5b988a4d0f7c74757bff9853939b135d4
hoyen204 <hoyen204@gmail.com>
2022-12-27 23:41:35 +0700

    Add project files.

---
+++ EmbeddedSelenium/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace EmbeddedSelenium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabNewTab = new System.Windows.Forms.TabPage();
+            this.mainTabControl = new System.Windows.Forms.TabControl();
+            this.mainTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabNewTab
+            // 
+            this.tabNewTab.Location = new System.Drawing.Point(4, 22);
+            this.tabNewTab.Name = "tabNewTab";
+            this.tabNewTab.Padding = new System.Windows.Forms.Padding(3);
+            this.tabNewTab.Size = new System.Drawing.Size(1076, 485);
+            this.tabNewTab.TabIndex = 1;
+            this.tabNewTab.Text = "+";
+            this.tabNewTab.UseVisualStyleBackColor = true;
+            // 
+            // mainTabControl
+            // 
+            this.mainTabControl.Controls.Add(this.tabNewTab);
+            this.mainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTabControl.Location = new System.Drawing.Point(0, 0);
+            this.mainTabControl.Name = "mainTabControl";
+            this.mainTabControl.SelectedIndex = 0;
+            this.mainTabControl.Size = new System.Drawing.Size(1084, 511);
+            this.mainTabControl.TabIndex = 0;
+            this.mainTabControl.Selecting += new System.Windows.Forms.TabControlCancelEventHandler(this.mainTabControl_Selecting);
+            this.mainTabControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainTabControl_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 511);
+            this.Controls.Add(this.mainTabControl);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.mainTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabNewTab;
+        private System.Windows.Forms.TabControl mainTabControl;
+    }
+}
+
---
+++ EmbeddedSelenium/Form1.cs
@@ -0,0 +1,101 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmbeddedSelenium
+{
+    public partial class Form1 : Form
+    {
+        private const int TCM_SETMINTABWIDTH = 0x1300 + 49;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        public HashSet<int> listChrome = new HashSet<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+            SendMessage(mainTabControl.Handle, TCM_SETMINTABWIDTH, IntPtr.Zero, (IntPtr)16);
+        }
+
+        private void mainTabControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            var lastIndex = mainTabControl.TabCount - 1;
+            if (mainTabControl.GetTabRect(lastIndex).Contains(e.Location))
+            {
+                new Thread(() =>
+                {
+                    var guid = Guid.NewGuid().ToString();
+                    ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                    service.HideCommandPromptWindow = true;
+                    ChromeDriver driver = new ChromeDriver(service);
+                    driver.ExecuteScript($"document.title = '{guid}';");
+
+                    var driverHandle = IntPtr.Zero;
+                    do
+                    {
+                        foreach(var item in Process.GetProcessesByName("chrome").Where(x => !string.IsNullOrEmpty(x.MainWindowTitle)).ToList())
+                        {
+                            if (item.MainWindowTitle.Contains(guid))
+                            {
+                                driverHandle = item.MainWindowHandle;
+                                listChrome.Add(item.Id);
+                            }
+                        }
+                        Thread.Sleep(10);
+                    }
+                    while (driverHandle == IntPtr.Zero);
+
+                    mainTabControl.Invoke(new Action(() =>
+                    {
+                        mainTabControl.TabPages.Insert(lastIndex, $"New Tab {lastIndex}");
+                        mainTabControl.SelectedIndex = lastIndex;
+                    }));
+
+
+                    TabPage lastTab = mainTabControl.TabPages[lastIndex];
+                    lastTab.Invoke(new Action(() =>
+                    {
+                        lastTab.Text = $"Zalo {listChrome.Count}";
+                        SetParent(driverHandle, lastTab.Handle);
+                        MoveWindow(driverHandle, 0, 0, lastTab.Width, lastTab.Height, true);
+                    }));
+                    driver.Navigate().GoToUrl("https://chat.zalo.me");
+
+                })
+                { IsBackground = true }.Start();
+            }
+
+
+        }
+
+        private void mainTabControl_Selecting(object sender, TabControlCancelEventArgs e)
+        {
+            if (e.TabPageIndex == mainTabControl.TabCount - 1)
+                e.Cancel = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            listChrome.ToList().ForEach(x => Process.GetProcessById(x)?.Kill());
+        }
+    }
+}
